725d56451a4b3014b433f83ec5aecf6be30da7f5
Tread-ing01 <amipattnaik21@gmail.com>
2025-10-29 10:25:10 +0530

    marketing-description

--- src/main/java/nirmalya/aathithya/webmodule/lms/controller/PromotionMarketingController.java
+++ src/main/java/nirmalya/aathithya/webmodule/lms/controller/PromotionMarketingController.java
@@ -1,3 +1,4 @@
+
 package nirmalya.aathithya.webmodule.lms.controller;
 
 import java.util.Arrays;
@@ -221,12 +222,15 @@ import nirmalya.aathithya.webmodule.common.utils.JsonResponse;
 		            String startDate = (String) requestJsonData.get("startDate");
 		            String endDate = (String) requestJsonData.get("endDate");
 		            String status = (String) requestJsonData.get("status");
+                    String description = (String) requestJsonData.get("description");
+
 
 		            requestPayload.put("marketingId", marketingId);
 		            requestPayload.put("marketName", marketName);
 		            requestPayload.put("startDate", startDate);
 		            requestPayload.put("endDate", endDate);
 		            requestPayload.put("status", status);
+                    requestPayload.put("description", description);
 		            // documentName already added above if file present
 		        } else {
 		            logger.error("Invalid type provided: {}", type);
@@ -381,3 +385,4 @@ import nirmalya.aathithya.webmodule.common.utils.JsonResponse;
 //
 		
 }
+
